eba877b75919f2d3caa874a9a7a01a88d7ae75ac
camdentadhg <camdent@gmail.com>
2024-01-23 11:18:11 -0600

    still stuck

--- app.py
+++ app.py
@@ -1,23 +1,29 @@
 """Blogly application."""
 
-from flask import Flask
+from flask import Flask, request, render_template, redirect, flash, session
 from models import db, connect_db, User
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = "secret"
+app.config['DEBUT_TB_INTERCEPT_REDIRECTS'] = False
 
 debug=DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
 
+@app.route('/')
+def home_page():
+    """shows dummy home page"""
+    return render_template('home.html')
 
 
-## create flask app
+## create flask app that is connected to the database
 ## make a base template
 ## make a user listing page
 ## make a new user form page
